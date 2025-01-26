1f03092971fb93b1af349811145078f50e9f760a
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2025-01-26 17:51:17 +0200

    first commit

---
+++ app/main/MainScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function MainScreen() {
+    return (
+        <View>
+            <Text>
+                Вы вошли в аккаунт
+            </Text>
+        </View>
+    )
+}
